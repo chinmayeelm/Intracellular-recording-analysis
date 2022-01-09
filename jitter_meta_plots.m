a6bd19aea89277449931584dee987f9248a98330
chinmayeelm <chinmayeelm@gmail.com>
2022-01-09 16:48:43 +0530

    Minor changes

--- jitter_meta_plots.m
+++ jitter_meta_plots.m
@@ -1,82 +1,75 @@
 cmp = parula(10);
 for i=1:10
-    % diff_sd = max_sd - min_sd;
-    figure(1);
+    
+%     figure(1);
 %     hold on;
-    %     scatter( meta_struct(i).max_stim_sd, meta_struct(i).jitter, 10,cmp(i,:), 'filled');
-    scatterhistogram(meta_table.max_stim_sd{i}, meta_table.jitter{i}, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
-    xlabel('Max STD in stimulus patterns (m)');
-    ylabel('Jitter (s)');
-    % title('58');
+%     scatter( blwgn_jitter_meta(i).max_stim_sd, blwgn_jitter_meta(i).fidelity, 10,cmp(i,:), 'filled');
+%     %     scatterhistogram(meta_table.max_stim_sd{i}, meta_table.fidelity{i}, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
+%     xlabel('Max STD in stimulus patterns (m)');
+%     ylabel('Jitter (s)');
+    
     
     
     figure(2);
     hold on;
-    %     colormap turbo(11);
-%     histogram(meta_struct(i).jitter,'BinWidth', 1000/meta_struct(i).fs,'FaceColor',cmp(i,:));
-    histogram(meta_table.jitter{i},'BinWidth', 1000/meta_table.fs(i),'FaceColor',cmp(i,:));
+    
+    histogram(blwgn_jitter_meta(i).fidelity,'BinWidth', 1000/blwgn_jitter_meta(i).fs,'FaceColor',cmp(i,:));
+    %     histogram(meta_table.fidelity{i},'BinWidth', 1000/meta_table.fs(i),'FaceColor',cmp(i,:));
     xlabel('Jitter (ms)');
     ylabel('Occurances');
     xlim ([0 2]);
-% end    
-    %%
+    
+    
     figure(3);
-%     hold on;
-    %     scatter(meta_struct(i).slope, meta_struct(i).jitter, 10,cmp(i,:), 'filled');
-    scatterhistogram(meta_table.slope, meta_table.jitter, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
-    xlabel('Slope of spike triggering stimulus pattern (mm)');
+    hold on;
+    scatter(blwgn_jitter_meta(i).slope, blwgn_jitter_meta(i).fidelity, 10,cmp(i,:), 'filled');
+    %     scatterhistogram(meta_table.slope, meta_table.fidelity, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
+    xlabel('Slope of spike triggering stimulus pattern (mm/s)');
     ylabel('Jitter (ms)');
     
     figure(4);
-%     hold on;
-    %     scatter(meta_table.pos_amp, meta_struct(i).jitter, 10,cmp(i,:), 'filled');
-    scatterhistogram(meta_table.pos_amp, meta_table.jitter, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
-    xlabel('Amplitude of spike triggering stimulus pattern (mm/sec)');
+    hold on;
+    scatter(blwgn_jitter_meta(i).pos_amp, blwgn_jitter_meta(i).fidelity, 10,cmp(i,:), 'filled');
+    %     scatterhistogram(meta_table.pos_amp, meta_table.fidelity, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
+    xlabel('Amplitude of spike triggering stimulus pattern (mm)');
     ylabel('Jitter (ms)');
     
-    %     figure(5);
-    %     hold on;
-    % %     scatter(meta_struct(i).stim_pattern_r, meta_struct(i).jitter,10,cmp(i,:), 'filled');
-    % scatterhistogram(meta_struct(i).stim_pattern_r, meta_struct(i).jitter, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
-    %     xlabel('Correlation with STA');
-    %     ylabel('Jitter (ms)');
+%     figure(5);
+%     hold on;
+%     scatter(blwgn_jitter_meta(i).stim_pattern_r, blwgn_jitter_meta(i).fidelity,10,cmp(i,:), 'filled');
+%     % scatterhistogram(blwgn_jitter_meta(i).stim_pattern_r, blwgn_jitter_meta(i).fidelity, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
+%     xlabel('Correlation with STA');
+%     ylabel('Jitter (ms)');
     
     figure(6);
-%     hold on;
-    %     scatter(meta_struct(i).time_to_prev_spike, meta_struct(i).jitter, 10,cmp(i,:), 'filled');
-    scatterhistogram(meta_table.time_to_prev_spike, meta_table.jitter, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
+    hold on;
+    scatter(blwgn_jitter_meta(i).time_to_prev_spike, blwgn_jitter_meta(i).fidelity, 10,cmp(i,:), 'filled');
+    %     scatterhistogram(meta_table.time_to_prev_spike, meta_table.fidelity, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
     xlabel('Time to previous spike (ms)');
     ylabel('Jitter (ms)');
     
-    %     colormap turbo(11);
-    figure(7);
+       
+    
+%     figure(8);
 %     hold on;
-    %     scatter(meta_struct(i).baseline_FR, mean(meta_struct(i).jitter), 40,cmp(i,:), 'filled');
-    scatterhistogram(meta_table.baseline_FR, meta_table.jitter, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
-    xlabel('Baseline firing rate (Hz)');
-    ylabel('Jitter (ms)');
-    ylim ([0 2]);
+%     scatter(blwgn_jitter_meta(i).time_to_prev_spike_column, blwgn_jitter_meta(i).fidelity(2:end), 10,cmp(i,:), 'filled');
+% %     scatterhistogram(blwgn_jitter_meta(i).time_to_prev_spike_column, blwgn_jitter_meta(i).fidelity, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
+%     xlabel('Time to previous spiking event (ms)');
+%     ylabel('Jitter (ms)');
+%     %     ylim ([0 2]);
     
-    %     colormap turbo(11);
-    %     figure(8);
-    %     hold on;
-    % %     scatter(meta_struct(i).time_to_prev_spike_column, meta_struct(i).jitter(2:end), 10,cmp(i,:), 'filled');
-    % scatterhistogram(meta_struct(i).time_to_prev_spike_column, meta_struct(i).jitter, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
-    %     xlabel('Time to previous spiking event (ms)');
-    %     ylabel('Jitter (ms)');
-    %     ylim ([0 2]);
+%     figure(9);
+%     hold on;
+%     scatter(blwgn_jitter_meta(i).stim_ev_r, blwgn_jitter_meta(i).fidelity,10,cmp(i,:), 'filled');
+%     %     scatterhistogram(blwgn_jitter_meta(i).stim_ev_r, blwgn_jitter_meta(i).fidelity, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
+%     xlabel('Correlation with Eigen vector');
+%     ylabel('Jitter (ms)');
     
-    %     figure(9);
-    %     hold on;
-    % %     scatter(meta_struct(i).stim_ev_r, meta_struct(i).jitter,10,cmp(i,:), 'filled');
-    %     scatterhistogram(meta_struct(i).stim_ev_r, meta_struct(i).jitter, 'HistogramDisplayStyle','smooth', 'LineStyle', '-', 'MarkerSize', 10, 'MarkerStyle', 'o', 'MarkerFilled', 'on' );
-    %     xlabel('Correlation with Eigen vector');
-    %     ylabel('Jitter (ms)');
+%     figure(10);
+%     scatter(blwgn_jitter_meta(i).stim_pattern_freq, blwgn_jitter_meta(i).fidelity, 10,cmp(i,:), 'filled');
+%     xlabel('Max frequency in spike triggering stimulus pattern (Hz)');
+%     ylabel('Jitter (ms)');
+%     hold on;
+%     xlim([0 350]);
     
-    %     figure(10);
-    %     scatter(meta_struct(i).stim_pattern_freq, meta_struct(i).jitter, 10,cmp(i,:), 'filled');
-    %     xlabel('Max frequency in spike triggering stimulus pattern (Hz)');
-    %     ylabel('Jitter (ms)');
-    %     hold on;
-    
-% end
+end
