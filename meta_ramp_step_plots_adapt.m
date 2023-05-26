39aab275f8905c50773d758c0d40a90632b1124b
chinmayeelm <chinmayeelm@gmail.com>
2023-05-26 18:18:17 +0530

    Update meta_ramp_step_plots_adapt.m

--- meta_ramp_step_plots_adapt.m
+++ meta_ramp_step_plots_adapt.m
@@ -1,29 +1,40 @@
-rampData = readlines('allRampList.txt');
+cd 'E:\Recordings\LUTs';
+% rampData = readlines('validRampList.txt');
 % ignorefiles = [1 2 4 5 6 8 17 19 25 27 30 33 42 44 46];
 % rampData(ignorefiles, :) = [];
 % rampData = readlines('rampDataList.txt');
-stepData = readlines('stepDataList.txt');
-dataDirectory_ramp = extractBefore(rampData, '_');
-filename_ramp = extractAfter(rampData, "_");
+% dataDirectory_ramp = extractBefore(rampData, '_');
+% filename_ramp = extractAfter(rampData, "_");
+% nfiles = length(filename_ramp);
+
+stepData = readlines('allStepList.txt');
 dataDirectory_step = extractBefore(stepData, '_');
 filename_step = extractAfter(stepData, "_");
-nfiles = length(filename_ramp);
-adaptation_coeff = zeros(1, nfiles);
-rsquare = zeros(1, nfiles);
+nfiles = length(filename_step);
+
+% stairData = readlines('allStairList.txt');
+% dataDirectory_stair = extractBefore(stairData, '_');
+% filename_stair = extractAfter(stairData, "_");
+% nfiles = length(filename_stair);
+
+% adaptation_coeff = zeros(1, nfiles);
+% rsquare = zeros(1, nfiles);
 
 ignorefiles = [];
 
-for irow = 6%:nfiles
+for irow = 21%:nfiles
     
     irow
-%     close all;
+    close all;
     
 %     try
-    P_ramp = getStructP(dataDirectory_ramp(irow), filename_ramp(irow),0);
+%     [P_ramp,intendedStimulus] = getStructP(dataDirectory_ramp(irow), filename_ramp(irow),0);
     
-%     P_step = getStructP(dataDirectory_step(irow), filename_step(irow),0);
-    rampGCFRplots(P_ramp);
-%     stepGCFRplots(P_step);
+    [P_step, intendedStimulus] = getStructP(dataDirectory_step(irow), filename_step(irow),0);
+%     [P_stair, intendedStimulus] = getStructP(dataDirectory_stair(irow), filename_stair(irow),0);
+%     rampGCFRplots(P_ramp, intendedStimulus);
+    stepGCFRplots(P_step);
+%     stairGCFRplots(P_stair, intendedStimulus);
 %     try
 %     [adaptation_coeff(irow), rsquare(irow)] = calcAdaptation(P_ramp);
 %     catch
