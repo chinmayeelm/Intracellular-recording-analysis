45d9a6464cb8a3ca0169f8ab75bb6d094fd43fa0
Chinmayee <chinmayeelm@gmail.com>
2024-03-01 11:34:12 +0530

    Update sdfill.m

--- sdfill.m
+++ sdfill.m
@@ -4,12 +4,14 @@ function sdfill(time,meanMovement, sd, c)
 
 curve1 = meanMovement + sd;
 curve2 = meanMovement - sd;
-x2 = [time' fliplr(time')];
+time = reshape(time, 1,[]);
+x2 = [time fliplr(time)];
 inBetween = [curve1 fliplr(curve2)];
-% fill(x2, inBetween, c, 'FaceAlpha', 0.2, 'EdgeColor', 'none');
 hold on;
-plot(time, meanMovement, 'Color', c, 'LineWidth', 1);
-plot(time, curve1, 'Color', c, 'LineWidth', 1);
-plot(time, curve2, 'Color', c, 'LineWidth', 1);
+fill(x2, inBetween, c, 'FaceAlpha', 0.3, 'EdgeColor', 'none');
+
+plot(time, meanMovement, 'Color', c, 'LineWidth', 1); 
+% plot(time, curve1, 'Color', c, 'LineWidth', 1);
+% plot(time, curve2, 'Color', c, 'LineWidth', 1);
 end
 
