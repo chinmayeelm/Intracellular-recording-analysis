668c14345fd71b17fa7ef0654c1e031cf8ed3ee2
Chinmayee <chinmayeelm@gmail.com>
2024-03-01 11:34:09 +0530

    Update savefigures.m

--- savefigures.m
+++ savefigures.m
@@ -1,23 +1,32 @@
 % function savefigures(filename, stim_name, figurehandle, date)
-function savefigures(P, addlabel, figureHandle, imageFlag)
+function savefigures(P, addlabel, figureHandle, imageFlag, dir_path)
     
 %     dir_path = 'F:\Chinmayee\Work\Recordings\Intracellular recording\analysis';
-    dir_path = 'D:\Work\Figures for presentation';
+   
     cd(dir_path)
 %     svg_name = sprintf("%s_%s_%s", string(join(split(date,'.'),'_')), filename, stim_name);
 %     prompt = 'Enter file name';
 %     svg_name = input(prompt);
-    title(gca, "")
-    if imageFlag == 0
+    % title(gca, "")
+    if imageFlag == "svg" 
         ext = '.pdf';
         imageType = "vector";
+    
     else
         ext = '.png';
         imageType = "image";
     end
     
-    filename = join([string(P(1).date) "_" P(1).filename "_" addlabel ext], "")
+    filename = join([string(P.date) "_" P.filename "_" P.stim_name "_" addlabel ext], "");
+    % filename = join([string(P.date) "_" P.filename "_" addlabel ext], "");
 %     imageType = 'image'
+    if imageFlag == "fig"
+        ext = ".fig";
+        filename = join([string(P.date) "_" P.filename "_" P.stim_name "_" addlabel ext], "");
+        % filename = join([string(P.date) "_" P.filename "_" addlabel ext], "");
+        savefig(figureHandle, filename, "compact");
+        return
+    end
     exportgraphics(figureHandle, filename, 'ContentType', imageType, 'Resolution',150);
 %     fig_name = sprintf("%s_%s.fig", filename, stim_name);
     
