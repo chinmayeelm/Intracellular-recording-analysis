eed658a9d2facc66afd5f81e1c2aa702b537b05f
Chinmayee <chinmayeelm@gmail.com>
2024-03-01 11:22:41 +0530

    Correcting the sign of stimulus
    The HES output is opposite in direction by default. The sign of the stimulus was corrected other functions. Sometimes, the sign was not corrected and the stimulus would be wrong. Fixing it here so that the output stimulus is ready to use.

--- butter_filtfilt.m
+++ butter_filtfilt.m
@@ -5,7 +5,7 @@ function filtered_antennal_movement  = butter_filtfilt(data, fc, fs, order,a, b,
     [x, y] = butter(order, fc/(fs/2), 'low');
 %     fvtool(x,y);
 
-    filtered_antennal_movement = filtfilt(x, y, antennal_movement);
+    filtered_antennal_movement = filtfilt(x, y, -antennal_movement);
 
 %     lpFilt = designfilt('lowpassiir','FilterOrder',order, ...
 %              'PassbandFrequency',fc,'PassbandRipple',0.05, ...
